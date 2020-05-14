f030cd2ec3f73400fcfbd21cb016408ae77939d6
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-05-15 01:40:54 +0300

    аккордеон доделать

--- script.js
+++ script.js
@@ -3,27 +3,45 @@ window.addEventListener('DOMContentLoaded', function() {
 
 //header&footer
     const headerFooter = () => {
-        const callBtn = document.querySelector('.call-btn'),
-            popUp = document.querySelector('popup-call');
-            console.log('popUp: ', popUp);
+        const callBtn = document.querySelectorAll('.call-btn'),
+            popUp = document.querySelector('.popup-call');
 
-        console.log(callBtn);
-
-        const showPopUp = () => {
-            popUp.classList.toggle('active-menu');
-        };
-        
-        callBtn.addEventListener('click', (event) => {
+        let opacity = 0;
+        popUp.style.opacity = opacity;
+        callBtn.forEach((elem) => {
+            elem.addEventListener('click', function animation(){
+                popUp.style.display = 'block';
+                opacity += 0.1;
+                popUp.style.opacity = opacity;
+                if(opacity < 1){
+                    setTimeout(animation, 20);
+                }
+            });
+        });
+        popUp.addEventListener('click', (event) => {
             let target = event.target;
-                target = target.closest('.popup-call');
-            if(!target){
-                showPopUp();
-            }    
+
+            if(target.classList.contains('popup-close')){
+                popUp.style.display = 'none';
+                return (opacity = 0);
+            } else {
+                target = target.closest('.popup-content');
+                if(!target){
+                    popUp.style.display = 'none';
+                }
+            }
         });
 
 
     };
     headerFooter();
+
+    //аккордеон
+    const accordion = () => {
+        const panel = document.querySelectorAll('.panel-default');
+        console.log(panel);
+    };
+    accordion();
 
 
 
